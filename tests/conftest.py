53b2c14be8fcf2e1b01c4eebeb7ff654b8ae06a8
Nikita <nikita.volnyagin@yandex.ru>
2024-02-07 23:44:00 +0400

    #10/2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
 import pytest
 from selene import browser
+from selenium import webdriver
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_configs():
+def browser_config():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
-    browser.config.window_height = 1080
+    driver_options = webdriver.ChromeOptions()
+    driver_options.add_argument('--headless')
+    browser.config.driver_options = driver_options
+    browser.config.window_width = 1900
+    browser.config.window_height = 950
 
     yield
+
     browser.quit()
