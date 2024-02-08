f0b7b757cd60adf5bbe43b785bc600ca8aba4000
Nikita <nikita.volnyagin@yandex.ru>
2024-02-08 09:26:04 +0400

    #10/3

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,17 @@
-import os
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import browser
-
 from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management(request):
+def setup_browser(request):
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1920
+    browser.config.window_width = 1900
     browser.config.window_height = 1080
     options = Options()
+
     selenoid_capabilities = {
         "browserName": "chrome",
         "browserVersion": "100.0",
@@ -30,8 +28,7 @@ def browser_management(request):
     )
 
     browser.config.driver = driver
-
-    yield
+    yield browser
 
     attach.add_screenshot(browser)
     attach.add_logs(browser)
